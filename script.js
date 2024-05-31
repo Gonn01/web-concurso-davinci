8434c1a476c4f90b9825080aed4e4d1d43fd3ddc
Gonzalo Rigoni <gonzalomiguelrigoni@gmail.com>
2024-05-31 20:46:12 -0300

    avances

--- script.js
+++ script.js
@@ -1,29 +1,40 @@
-window.onload = function () {
-  const secciones = document.getElementById("secciones");
+let categorias = [
+  {
+    title: "Manga",
+    description: "Todos los títulos que necesitas",
+    img1: "imgs/manga1.png",
+    img2: "imgs/manga2.png",
+    img3: "imgs/manga3.png",
+  },
+  {
+    title: "Novelas juveniles",
+    description: "Todos los títulos de novela para adolescentes",
+    img1: "imgs/novelas_juveniles1.png",
+    img2: "imgs/novelas_juveniles2.png",
+    img3: "imgs/novelas_juveniles3.png",
+  },
+  {
+    title: "Bienestar personal",
+    description: "Todos los títulos para bienestar personal",
+    img1: "imgs/bienestar_personal1.png",
+    img2: "imgs/bienestar_personal2.png",
+    img3: "imgs/bienestar_personal3.png",
+  },
+];
+let itemEnCarrito = 0;
 
-  let categorias = [
-    {
-      title: "Manga",
-      description: "Todos los títulos que necesitas",
-      img1: "imgs/manga1.png",
-      img2: "imgs/manga2.png",
-      img3: "imgs/manga3.png",
-    },
-    {
-      title: "Novelas juveniles",
-      description: "Todos los títulos de novela para adolescentes",
-      img1: "imgs/novelas_juveniles1.png",
-      img2: "imgs/novelas_juveniles2.png",
-      img3: "imgs/novelas_juveniles3.png",
-    },
-    {
-      title: "Bienestar personal",
-      description: "Todos los títulos para bienestar personal",
-      img1: "imgs/bienestar_personal1.png",
-      img2: "imgs/bienestar_personal2.png",
-      img3: "imgs/bienestar_personal3.png",
-    },
-  ];
+document.addEventListener("DOMContentLoaded", function () {
+  // Tu código JavaScript aquí
+  function sumarCarrito() {
+    itemEnCarrito++;
+    const carritoValorElement = document.querySelector("#carrito-valor");
+    carritoValorElement.textContent = itemEnCarrito;
+  }
+});
+window.onload = function () {
+  const carrito = document.getElementById("carrito-valor");
+  carrito.textContent = itemEnCarrito;
+  const secciones = document.getElementById("secciones");
 
   categorias.forEach((categoria, index) => {
     const seccion = document.createElement("section");
